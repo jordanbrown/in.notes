1dbbcc08a8229c432b8190ac874eac08d35ee471
Michael Babiy <ic.babiy@gmail.com>
2014-05-05 08:13:14 -0700

    Add comments to the INNotesFetchedResultsController.

--- in.notes/INNotesFetchedResultsController.h
+++ in.notes/INNotesFetchedResultsController.h
@@ -6,6 +6,17 @@
 //  Copyright (c) 2014 in.notes. All rights reserved.
 //
 
+/**
+ *  INNotesFetchedResultsController is a subclass of INTableViewController
+ *  and is responsible for managing an instance of NSFetchedResultsController.
+ *  Any ViewController (TableViewController) that requires interaction with
+ *  CoreData should inherit from this VC. 
+ 
+ *  In addition to interaction with CoreData, this VC invalidates cell height
+ *  size cache once controller:didChangeObject:atIndexPath:forChangeType:newIndexPath: This might change
+ *  in the near future though.
+ */
+
 #import "INTableViewController.h"
 
 @interface INNotesFetchedResultsController : INTableViewController <NSFetchedResultsControllerDelegate>
