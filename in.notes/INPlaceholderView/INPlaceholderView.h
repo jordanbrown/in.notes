73185b6f51eee51156432657ab3269bca5e8505f
Michael Babiy <ic.babiy@gmail.com>
2014-05-07 12:53:08 -0700

    Add INPlaceholderView.

---
+++ in.notes/INPlaceholderView/INPlaceholderView.h
@@ -0,0 +1,13 @@
+//
+//  INPlaceholderView.h
+//  in.notes
+//
+//  Created by iC on 5/7/14.
+//  Copyright (c) 2014 in.notes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface INPlaceholderView : UIView
+
+@end
---
+++ in.notes/INPlaceholderView/INPlaceholderView.m
@@ -0,0 +1,31 @@
+//
+//  INPlaceholderView.m
+//  in.notes
+//
+//  Created by iC on 5/7/14.
+//  Copyright (c) 2014 in.notes. All rights reserved.
+//
+
+#import "INPlaceholderView.h"
+
+@implementation INPlaceholderView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
