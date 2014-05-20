15abf8d8a945666c82057a065af9d638df7e9981
Michael Babiy <ic.babiy@gmail.com>
2014-05-20 13:09:01 -0700

    Add INEditViewController and set it up in Storyboard.

---
+++ in.notes/INEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  INEditViewController.h
+//  in.notes
+//
+//  Created by iC on 5/20/14.
+//  Copyright (c) 2014 in.notes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface INEditViewController : UIViewController
+
+@end
---
+++ in.notes/INEditViewController.m
@@ -0,0 +1,27 @@
+//
+//  INEditViewController.m
+//  in.notes
+//
+//  Created by iC on 5/20/14.
+//  Copyright (c) 2014 in.notes. All rights reserved.
+//
+
+#import "INEditViewController.h"
+
+@interface INEditViewController ()
+
+@end
+
+@implementation INEditViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
