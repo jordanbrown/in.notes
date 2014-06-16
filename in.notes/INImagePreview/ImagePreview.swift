109230401a7d6c5d6cbdb0009c8b98be51bdcb13
mbabiy <mbabiy@gettyimages.com>
2014-06-15 20:40:28 -0700

    Add ImagePreview swift file.

---
+++ in.notes/INImagePreview/ImagePreview.swift
@@ -0,0 +1,27 @@
+//
+//  ImagePreview.swift
+//  in.notes
+//
+//  Created by Michael Babiy on 6/15/14.
+//  Copyright (c) 2014 in.notes. All rights reserved.
+//
+
+import UIKit
+
+class ImagePreview: UIView {
+
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        // Initialization code
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
