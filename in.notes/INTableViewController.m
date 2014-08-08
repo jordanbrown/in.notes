4aa72eeed120281089f8c776ef3fd211da67c1bb
Michael Babiy <ic.babiy@gmail.com>
2014-08-07 17:48:56 -0700

    Add iOS 8 feature for table view cells separation.

--- in.notes/INTableViewController.m
+++ in.notes/INTableViewController.m
@@ -18,6 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.tableView setSeparatorEffect:[UIVibrancyEffect effectForBlurEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleLight]]];
 }
 
 - (void)didReceiveMemoryWarning {
